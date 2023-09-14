94f9e4422b7312c76593c58ac3d0226e68d1d230
Nuttapon Buaban <nuttapon.dev@gmail.com>
2023-09-14 15:39:39 +0700

    Initial Commit

---
+++ AnimationExperiment/AnimationExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationExperimentApp.swift
+//  AnimationExperiment
+//
+//  Created by Nuttapon Buaban on 14/9/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct AnimationExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
